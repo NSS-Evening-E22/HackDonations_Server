f22824b004ab5a65ffe3d6d05602cbd890e18a15
rtullis98 <121829715+rtullis98@users.noreply.github.com>
2023-10-23 21:14:33 -0500

    Made Organization, Tag, and User classes in Models folder.

---
+++ Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿namespace HackDonations_Server.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<string> OrganizationList { get; set; }
+
+    }
+}
