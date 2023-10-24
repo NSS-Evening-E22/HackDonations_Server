571a67324ebb1968a13ef05bad8883186c882612
rtullis98 <121829715+rtullis98@users.noreply.github.com>
2023-10-24 18:11:47 -0500

    Update Tag.cs

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,12 +1,14 @@
-﻿namespace HackDonations_Server.Models
+using System.Text.Json.Serialization;
+
+namespace HackDonations_Server.Models
 {
     public class Tag
     {
-        public int Id { get; set; }
+        public int id { get; set; }
 
-        public string Name { get; set; }
+        public string name { get; set; }
 
-        public List<Tag> TagList { get; set; }
+        public List<Tag> tagList { get; set; }
 
     }
 }
